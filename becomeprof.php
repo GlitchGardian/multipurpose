e7d812e3bfcb427c9a8cfc8387653d9ca2247d79
GlitchGardian <abhisheknbhatt394@gmail.com>
2023-12-31 20:33:39 +0530

    added

--- becomeprof.php
+++ becomeprof.php
@@ -1,39 +1,37 @@
 <?php
 
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $city = $_POST['city'];
-    $address = $_POST['address'];
-    $service = $_POST['service'];
-    $description = $_POST['description'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$city = $_POST['city'];
+$address = $_POST['address'];
+$service = $_POST['service'];
+$description = $_POST['description'];
 
-    $file_name = $_FILES['profile_photo']['name'];
-    $tmp_name = $_FILES['profile_photo']['tmp_name'];
-    $target_folder = 'profilephotos/'.$file_name;
-    
-        
-    if (move_uploaded_file($tmp_name, $target_folder)) {
-        echo 'File Uploaded Successfully';
-    } else {
-        echo 'Error Uploading File';
-    }
+$file_name = $_FILES['profile_photo']['name'];
+$tmp_name = $_FILES['profile_photo']['tmp_name'];
+$target_folder = 'profilephotos/' . $file_name;
 
 
-     $conn = mysqli_connect("localhost","root","","multipurpose");
+if (move_uploaded_file($tmp_name, $target_folder)) {
+    echo 'File Uploaded Successfully';
+} else {
+    echo 'Error Uploading File';
+}
 
-    if ($conn){
-    
-         $sql = "INSERT INTO professionals (profile_photo,name, email, phone, city, address, service, description) VALUES ('$target_folder','$name', '$email', '$phone','$city', '$address', '$service', '$description')";
-    
-         $run = mysqli_query($conn,$sql);    
 
-         header('Location: becomeresponse.html');
-        
-        }
+$conn = mysqli_connect("localhost", "root", "", "multipurpose");
 
-        else{       
-            echo "error". mysqli_error($conn);
-        }
-        exit(); 
+if ($conn) {
+
+    $sql = "INSERT INTO professionals (profile_photo,name, email, phone, city, address, service, description) VALUES ('$target_folder','$name', '$email', '$phone','$city', '$address', '$service', '$description')";
+
+    $run = mysqli_query($conn, $sql);
+
+    header('Location: becomeresponse.html');
+
+} else {
+    echo "error" . mysqli_error($conn);
+}
+exit();
 ?>
